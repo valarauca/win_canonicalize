1b1280cc85377dafbfff801bfba4376202c21ca9
wcl <codylaeder@gmail.com>
2021-09-17 15:04:59 -0700

    Initial Commit

---
+++ build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    windows::build!(
+        Windows::Win32::Foundation::PWSTR,
+        Windows::Win32::UI::Shell::PathCchCanonicalizeEx,
+        Windows::Win32::System::Com::CoInitialize,
+    );
+}
